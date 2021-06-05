e17f6563f6b8bddd161ad676daaf480cfd111292
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-06-05 18:27:51 -0500

    Jump!

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector3 moveDirection = Vector3.zero;
+    public CharacterController player;
+    public float speed = .1f;
+    public float jump_force = 6.0f;
+    public float gravity = 6.0f;
+
+    private void start()
+    {
+        player = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float x_direction = Input.GetAxis("Horizontal");
+        float z_direction = Input.GetAxis("Vertical");
+
+        if (player.isGrounded)
+        {
+            moveDirection = new Vector3(x_direction, 0.0f, z_direction);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+                moveDirection.y = jump_force;
+        }
+        else
+        {
+            moveDirection = new Vector3(x_direction, moveDirection.y, z_direction);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection.x *= speed;
+            moveDirection.z *= speed;
+        }
+        moveDirection.y -= (gravity * Time.deltaTime);
+        player.Move(moveDirection * Time.deltaTime);
+    }
+}
