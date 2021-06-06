81842528a69ea4d3c36a25d49b67ffa1ed7d7098
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-06-05 23:26:55 -0500

    Rotate the camera on their own by moving the mouse

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    public float vertical_speed = 1.0f;
+    public float horizontal_speed = 1.0f;
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+    
+
+    void Update()
+    {
+        yaw += horizontal_speed * Input.GetAxis("Mouse X");
+        pitch += vertical_speed * Input.GetAxis("Mouse Y");
+
+        // Rotate the camera on their own by moving the mouse.
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        
+        // The camera to follow the player.
+        transform.position = target.position + offset;
+    }
+}
