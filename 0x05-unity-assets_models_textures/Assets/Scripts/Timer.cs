02dbae5b0ffca500730cbf83422f8e047376e71e
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-06-06 12:34:13 -0500

    Minutes, seconds & milliseconds

--- 0x05-unity-assets_models_textures/Assets/Scripts/Timer.cs
+++ 0x05-unity-assets_models_textures/Assets/Scripts/Timer.cs
@@ -4,15 +4,16 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour
 {
     public Text TimerText;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float timer, minutes, seconds, milliseconds;
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        minutes = (timer / 60);
+        seconds = (timer % 60);
+        milliseconds = (timer * 100 % 100);
+
+        TimerText.text = string.Format("{0:0}:{1:00}.{2:00}", minutes, seconds, milliseconds);
     }
 }
